43e3d6abb827d3c6cc602dbbc7c67892af791223
laurence <lj.villanueva2918@gmail.com>
2022-03-25 13:47:38 +0800

    done activity

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,113 +1,109 @@
 import React from "react";
 import { View, Image, Text, Dimensions } from "react-native";
-import LottieView from 'lottie-react-native';
-import { Provider as PaperProvider, TextInput, Button, Portal, Modal} from 'react-native-paper';
+import { Provider as PaperProvider, TextInput, Button, Portal, Modal } from "react-native-paper";
 
 export default function TabOneScreen() {
-  const [username, inputUsername] = React.useState("");
-  const [password, inputPassword] = React.useState("");
-  const [sVisible, setsVisible] = React.useState(false);
-  const [fVisible, setfVisible] = React.useState(false);
-  const showModal = () => setsVisible(true);
-  const hideModal = () => setsVisible(false);
-  const showModalF = () => setfVisible(true);
-  const hideModalF = () => setfVisible(false);
-  
-  const containerStyle = {backgroundColor: 'white', padding: 20}
+    // Modals
+    const [visible, setVisible] = React.useState(false);
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+    const containerStyle = { backgroundColor: "white", padding: 20 };
+    const [register, setRegister] = React.useState("Registered!");
 
-  return (
-    
-    <PaperProvider>
-
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#fff',
-        justifyContent: 'center',
-        paddingHorizontal: 20
-      }}
-    >
-      <Portal>
-        <Modal visible={sVisible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-          <Text>Success</Text>
-        </Modal>
-      </Portal>
-
-      <Portal>
-        <Modal visible={fVisible} onDismiss={hideModalF} contentContainerStyle={containerStyle}>
-          <Text>Failed</Text>
-        </Modal>
-      </Portal>
-
-      <View style={{
-        flex: 1,
-      }}>
-        <LottieView
-          source={require('../assets/lottie/47759-process-cellphone-error.json')}
-          autoPlay={true}
-          loop={true}
-          style={{
-            flex: 1,
-          }}
-        />
-      </View>
-
-      <View style={{
-        flex: 1,
-      }}>
-        <View style={{
-          flex: 0
-        }}>
-          <TextInput
-            mode="outlined"
-            label="Email"
-            value={username}
-            onChangeText={newUsername => inputUsername(newUsername)}
-            autoComplete = {false}
-          />
-        </View>
-
-        <View style={{
-          flex: 0,
-          marginBottom: 40
-        }}>
-          <TextInput
-            mode="outlined"
-            label="Password"
-            value={password}
-            secureTextEntry={true}
-            onChangeText={newPassword => inputPassword(newPassword)}
-            autoComplete = {false}
-            style={{
-              marginTop:20}}
-          />
-        </View>
-        
-        <View style={{
-          flex: 0,
-        }}>
-
-          <View style={{
-            flex: 0
-          }}>
-            <Button mode="outlined" onPress={() => {
-              if(username === "admin" && password === "password"){
-              showModal();
-              } else{
-                showModalF();
-              }
-            }}>
-              Login
-            </Button>
-
-          </View>
-
-        </View>
-
-      </View>
-
-    </View>
-
-    </PaperProvider>
-  );
+    // Username || Password
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    return (
+        <PaperProvider>
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: "#fff",
+                    justifyContent: "center",
+                    paddingHorizontal: 20,
+                }}
+            >
+                <Portal>
+                    <Modal
+                        visible={visible}
+                        onDismiss={hideModal}
+                        contentContainerStyle={containerStyle}
+                    >
+                        <Text>{register}</Text>
+                    </Modal>
+                </Portal>
+                <View
+                    style={{
+                        flex: 0,
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: "black",
+                            fontSize: 40,
+                        }}
+                    >
+                        Register
+                    </Text>
+                    <Text
+                        style={{
+                            color: "black",
+                        }}
+                    >
+                        Enter credential to create new account.
+                    </Text>
+                </View>
+                <View
+                    style={{
+                        flex: 0,
+                        marginTop: 20,
+                    }}
+                >
+                    <TextInput
+                        mode="outlined"
+                        label="Username"
+                        value={username}
+                        onChangeText={(newUsername) => setUsername(newUsername)}
+                        autoComplete={false}
+                    />
+                </View>
+                <View
+                    style={{
+                        flex: 0,
+                    }}
+                >
+                    <TextInput
+                        mode="outlined"
+                        label="Password"
+                        value={password}
+                        secureTextEntry={true}
+                        onChangeText={(newPassword) => setPassword(newPassword)}
+                        autoComplete={false}
+                    />
+                </View>
+                <View
+                    style={{
+                        flex: 0,
+                        paddingTop: 20,
+                        paddingHorizontal: 50,
+                    }}
+                >
+                    <Button
+                        mode="outlined"
+                        onPress={() => {
+                            if (username !== "" && password !== "") {
+                                setRegister("Register Success.");
+                                showModal();
+                            } else {
+                                setRegister("Incomplete credential, check username and password.");
+                                showModal();
+                            }
+                        }}
+                    >
+                        Register
+                    </Button>
+                </View>
+            </View>
+        </PaperProvider>
+    );
 }
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,31 +1,101 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React from "react";
+import { View, Image, Text, Dimensions } from "react-native";
+import LottieView from 'lottie-react-native';
+import { Provider as PaperProvider, TextInput, Button, Portal, Modal} from 'react-native-paper';
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    const [username, inputUsername] = React.useState("");
+    const [password, inputPassword] = React.useState("");
+    const [sVisible, setsVisible] = React.useState(false);
+    const [fVisible, setfVisible] = React.useState(false);
+    const showModal = () => setsVisible(true);
+    const hideModal = () => setsVisible(false);
+    const showModalF = () => setfVisible(true);
+    const hideModalF = () => setfVisible(false);
+  
+    const containerStyle = {backgroundColor: 'white', padding: 20}
+
+    return (
+    
+        <PaperProvider>
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: '#fff',
+                    justifyContent: 'center',
+                    paddingHorizontal: 20
+                }}
+            >
+                <Portal>
+                    <Modal visible={sVisible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                        <Text>Success</Text>
+                    </Modal>
+                </Portal>
+                <Portal>
+                    <Modal visible={fVisible} onDismiss={hideModalF} contentContainerStyle={containerStyle}>
+                        <Text>Failed</Text>
+                    </Modal>
+                </Portal>
+            <View style={{
+                  flex: 1,
+                }}>
+            <LottieView
+                source={require('../assets/lottie/47759-process-cellphone-error.json')}
+                autoPlay={true}
+                loop={true}
+                style={{
+                  flex: 1,
+                }}
+            />
+            </View>
+            <View style={{
+                flex: 1,
+                }}>
+                <View style={{
+                    flex: 0
+                    }}>
+                    <TextInput
+                        mode="outlined"
+                        label="Email"
+                        value={username}
+                        onChangeText={newUsername => inputUsername(newUsername)}
+                        autoComplete = {false}
+                    />
+            </View>
+            <View style={{
+                flex: 0,
+                marginBottom: 40
+                }}>
+                    <TextInput
+                        mode="outlined"
+                        label="Password"
+                        value={password}
+                        secureTextEntry={true}
+                        onChangeText={newPassword => inputPassword(newPassword)}
+                        autoComplete = {false}
+                        style={{
+                          marginTop:20}}
+                    />
+            </View>
+            <View style={{
+                flex: 0,
+                }}>
+                <View style={{
+                    flex: 0
+                    }}>
+                    <Button mode="outlined" onPress={() => {
+                        if(username === "admin" && password === "password"){
+                        showModal();
+                        } else{
+                          showModalF();
+                        }
+                        }}>
+                          Login
+                    </Button>
+          </View>
+        </View>
+      </View>
     </View>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
